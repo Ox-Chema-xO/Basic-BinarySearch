a4690e02e220e6a251713086503f71156b76681e
Diego <manuelchema999@gmail.com>
2025-04-07 23:15:01 -0500

    add bubble-sort for sorted the array

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def main():
-    print("Hi, this is a implementation of binary search")
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - 1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
+
+def main():
+    arr = [24,6,12,9,14,1,20,7]
+    x = 9 #elemento a buscar
+    print(f"Arreglo al inicio:{arr}")
+    print(f"Arreglo ordenado:{bubble_sort(arr)}")
+    
 if __name__ == "__main__":
     main()
